5f9da12065c47342c288af9382882bb4f72716df
Gaganpreet Khurana <giftykhurana0@gmail.com>
2022-02-14 23:30:36 +0530

    Right Move Done Working on compiling it

--- src/board.java
+++ src/board.java
@@ -1,4 +1,7 @@
+package Game2048;
 import java.util.Random;
+import java.util.Vector;
+
 public class board {
     public int matrix[4][4];
     Random randomObject;
@@ -26,7 +29,7 @@ public class board {
     }
     int randomNumber(){
         if(this.randomObject.nextBoolean()){
-            return 3;
+            return 2;
         }
         else{
             return 4;
@@ -35,5 +38,40 @@ public class board {
     int randomPosition(Vector<int> postions){
         int range=postions.size();
         return postions[this.randomObject.nextInt(range)];
+    }
+    void rightMoveRow(int row){
+        int j=3;
+        for(int i=2;i>=0;i--){
+            if(j==i){
+                break;
+            }
+            while(matrix[row][j]!=0 && matrix[row][j]!=matrix[row][i]){
+                j--;
+            }
+            if(matrix[row][j]==matrix[row][i]){
+                matrix[row][j]*=2;
+            }
+            else{
+                matrix[row][j]=matrix[row][i];
+            }
+            matrix[row][i]=0;
+            j--;
+        }
+    }
+    void rightMove(){
+        for(int i=0;i<4;i++){
+            rightMoveRow(i);
+        }
+        int randomNumber=randomNumber();
+        Vector<int> possiblePositions = new Vector<int>();
+        for(int i=0;i<4;i++){
+            if(matrix[i][0]==0){
+                possiblePositions.append(i);
+            }
+        }
+        if(possiblePositions.size()>0){
+            int randomPosition= randomPosition(possiblePositions);
+            matrix[randomPosition][0]=randomNumber;
+        }
     }
 }
--- src/game.java
+++ src/game.java
@@ -1,6 +1,9 @@
+package Game2048;
+import .board.*;
 class game{
 
     public static void main(String[] args) {
-
+        board newBoard= new board();
+        newBoard.printBoard();
     }
 }
