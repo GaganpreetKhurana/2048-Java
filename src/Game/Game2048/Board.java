c2b3c17ff304c82221c7d29b4702ec19aad4247d
Gaganpreet Khurana <54024846+GaganpreetKhurana@users.noreply.github.com>
2022-03-15 17:45:51 +0530

    Packages erros fixed

--- src/Game/Game2048/Board.java
+++ src/Game/Game2048/Board.java
@@ -1,9 +1,8 @@
-//package Game2048;
+package Game2048;
 
 import java.util.LinkedList;
-import java.util.InputMismatchException;
 import java.util.Random;
-import java.util.Scanner;
+
 public class Board {
 
     private int[][] matrix; // Board matrix
@@ -217,73 +216,5 @@ public class Board {
 
     public boolean checkGameFinish(){
         return emptyCells.size()==0;
-    }
-
-    private static void clearConsole(){
-        // to clear console
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-
-        System.out.println("Welcome to 2048!!!!");
-        System.out.println("Moves: L(Left) R(Right) U(Up) D(Down)\nPress any other key to end :-(\nPress ENTER key after every move");
-
-    }
-
-
-
-    public static void main(String[] args) {
-
-        clearConsole();
-
-        // new Board Object
-        Board newBoard= new Board();
-        
-
-        // Scanner object for input
-        Scanner scan = new Scanner(System.in);
-
-
-        while (!newBoard.checkGameFinish()) {
-            try {
-                System.out.print("Move: ");
-                String input = scan.nextLine();
-
-                // call appropriate function according to input
-                switch (input){
-                    case "L":
-                        newBoard.leftMove();
-                        break;
-                    case "R":
-                        newBoard.rightMove();
-                        break;
-                    case "U":
-                        newBoard.upMove();
-                        break;
-                    case "D":
-                        newBoard.downMove();
-                        break;
-                    default:
-                        // exit
-                        newBoard.printBoard();
-                        throw new InputMismatchException();
-                }
-
-                clearConsole();
-                newBoard.printBoard();
-            }
-            catch (InputMismatchException e) {
-                clearConsole();
-                System.out.print("Invalid Input!!\nThank You for Playing :-)\n");
-                newBoard.printBoard();
-                scan.close();
-                System.exit(0);
-            }
-        }
-        clearConsole();
-        System.out.print("The End!!\nThank You for Playing :-)\n");
-        newBoard.printBoard();
-        scan.close();
-        System.exit(0);
-
-    }
+    }    
 };
